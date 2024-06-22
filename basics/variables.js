328fba6dedde9d02c6456b2298763efb317e6625
MrTalhaXD <128725170+MrTalhaXD@users.noreply.github.com>
2024-06-22 19:50:28 +0000

    Basics done

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId="21345"
+let accountEmail="talha@gmail.com"
+var accountPassword="12345"
+accountCity="Faisalabad"
+
+//accountId="1234"
+accountEmail="123@gmail.com"
+accountPassword="54321"
+accountCity="Lahore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
